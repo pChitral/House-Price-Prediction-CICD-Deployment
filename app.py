bab560cfcb3b18d42edac0d237e283770f224f5f
pChitral <chitral.patil@vit.edu.in>
2023-04-07 03:16:23 -0400

    added from sklearn.preprocessing import StandardScaler to app.y

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pickle
 from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 # Load the model
